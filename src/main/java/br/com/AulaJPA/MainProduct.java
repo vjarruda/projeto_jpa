ef45caa0f6ddc972f8b49774693f251e44de0080
Vinícius Arruda <arrudavini25@gmail.com>
2024-12-09 16:19:23 -0300

    adiciona dao e main(incompleto)

---
+++ src/main/java/br/com/AulaJPA/MainProduct.java
@@ -0,0 +1,5 @@
+package br.com.AulaJPA;
+
+public class MainProduct {
+
+}
---
+++ src/main/java/br/com/AulaJPA/ProductDAO.java
@@ -0,0 +1,55 @@
+package br.com.AulaJPA;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ProductDAO {
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("crud-baisc");
+	EntityManager em = emf.createEntityManager();
+	
+	public void salvar(Product product) { 
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(product);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public void atualizar(Product product) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.merge(product);
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public void remover(Long id) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Product product = em.find(Product.class, id);
+		if(product != null) {
+			em.remove(product);
+		}
+		em.getTransaction().commit();
+		em.close();
+	}
+	
+	public List<Product> listar() {
+		EntityManager em = emf.createEntityManager();
+		List<Product> products = em.createQuery("FROM Product", Product.class).getResultList();
+		em.close();
+		return products;
+	}
+	
+	public Product buscarporId(Long id) {
+		EntityManager em = emf.createEntityManager();
+		Product product  = em.find(Product.class, id);
+		em.close();
+		return product;
+	}
+	
+	
+}
