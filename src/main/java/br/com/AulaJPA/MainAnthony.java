fa14adc38d46fb913e3659ffa07de17c69109699
Anthony Vinicius <91100881+AnthonyVinicius@users.noreply.github.com>
2024-12-12 23:37:58 -0300

    Update MainAnthony.java

--- src/main/java/br/com/AulaJPA/MainAnthony.java
+++ src/main/java/br/com/AulaJPA/MainAnthony.java
@@ -13,11 +13,18 @@ public class MainAnthony {
         Type typeProduct = new Type();
         typeProduct.setType("Product");
 
+        typeDAO.salvar(typeProduct);
+        typeDAO.salvar(typeService);
+
         Brand brand = new Brand();
         brand.setName("Petinho");
 
         Brand brand1 = new Brand();
         brand.setName("Ouro Verde");
+
+        brandDao.salvar(brand);
+        brandDao.salvar(brand1);
+
 
         AverageRate rate = new AverageRate();
 
